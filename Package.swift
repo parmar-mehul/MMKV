1cdc48e752756eada78c5b110d15567a3c4ad990
Mehul Parmar <mparmar.mailbox@gmail.com>
2024-07-09 14:41:40 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .binaryTarget(
             name: "MMKV",
 			url: "https://github.com/parmar-mehul/MMKV-XCFramework/releases/download/1.3.7/MMKV.xcframework.zip",
-            checksum: "87800757df268fe3d4a6bb27c03b8da5de393bea87ed927d59bd8a6a190f8425"
+            checksum: "279ec929a18f6cda7c42edc8b6f124ae28d274b6d1b01894acff1a4caae89058"
         ),
         .testTarget(name: "MMKVTests", dependencies: ["MMKV"]),
     ]
