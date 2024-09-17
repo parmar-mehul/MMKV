822c5c3c6b85e33b0431a4f6c1f6d359357804de
Mehul Parmar <mparmar.mailbox@gmail.com>
2024-09-17 19:49:41 +0530

    Updated to v1.3.9

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MMKV",
-			url: "https://github.com/parmar-mehul/MMKV-XCFramework/releases/download/1.3.7/MMKV.xcframework.zip",
-            checksum: "279ec929a18f6cda7c42edc8b6f124ae28d274b6d1b01894acff1a4caae89058"
+			url: "https://github.com/parmar-mehul/MMKV-XCFramework/releases/download/1.3.9/MMKV.xcframework.zip",
+            checksum: "a7fe3890fcbedc4a20ecdee4d7751fe48e85cf9d14fae632ddb259c97836881e"
         ),
         .testTarget(name: "MMKVTests", dependencies: ["MMKV"]),
     ]
--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # MMKV-XCFramework
 
 This repository hosts the MMKV iOS & macOS framwork as an XCFramework based Swift Package.
+It contains,
+- MMKV-iOS
+- MMKV-iOS-Simulator
+- MMKV-macOS-Catalyst
+- MMKV-watchOS
+- MMKV-tvOS
+- MMKV-tvOS-Simulator
 
-Use this repository's Swift Package in Xcode 12+ and Swift version 5.5+
-
+Use this repository's Swift Package in Xcode 15+ and Swift version 5.10+
