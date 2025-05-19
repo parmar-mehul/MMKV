1f6104970298e9bb92f812a6fd6abc2de89a1a6f
Mehul Parmar <mparmar.mailbox@gmail.com>
2025-05-19 15:40:34 +0530

    Update 2.2.2
    Released on 2025-05-19
    
    #### Updated
    - Updated CHANGELOG.md
    - Generated XCFramwork from MacOS 15.4.1 (24E263), XCode Version 16.3 (16E140) with
        - SDKROOT
            - iphoneos18.4, iphonesimulator18.4, appletvos18.4, appletvsimulator18.4, macosx15.4, macosx15.4, watchos11.4, watchsimulator11.4
    #### Added
    - Added support of https://github.com/Tencent/MMKV/releases/tag/v2.2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,9 +6,22 @@ All notable changes to this project will be documented in this file.
 
 #### 2.x Releases
 
-- `2.0.x` Releases - [2.1.0](#210) | [2.0.1](#201) | [2.0.0](#200)
+- `2.0.x` Releases - [2.2.2](#222) | [2.1.0](#210) | [2.0.1](#201) | [2.0.0](#200)
 
 ---
+
+## [2.2.2](https://github.com/parmar-mehul/MMKV/releases/tag/2.2.2)
+
+Released on 2025-05-19
+  
+#### Updated
+- Updated CHANGELOG.md
+- Generated XCFramwork from MacOS 15.4.1 (24E263), XCode Version 16.3 (16E140) with
+    - SDKROOT
+        - iphoneos18.4, iphonesimulator18.4, appletvos18.4, appletvsimulator18.4, macosx15.4, macosx15.4, watchos11.4, watchsimulator11.4
+#### Added
+- Added support of https://github.com/Tencent/MMKV/releases/tag/v2.2.2
+
 
 ## [2.1.0](https://github.com/parmar-mehul/MMKV/releases/tag/2.1.0)
 
--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MMKV",
-			url: "https://github.com/parmar-mehul/MMKV-XCFramework/releases/download/2.1.0/MMKV.xcframework.zip",
-            checksum: "137f3be6aad193a9195c4e484f34d06d7be713b4e0a1a81b32ca7fd55e274d5b"
+			url: "https://github.com/parmar-mehul/MMKV-XCFramework/releases/download/2.2.2/MMKV.xcframework.zip",
+            checksum: "c355bc52c0c2425c6a8f89bb3e73c24d3d3fd623c6a478ebdf979084f95b35b5"
         ),
 		.binaryTarget(
 			name: "MMKVWatchExtension",
-			url: "https://github.com/parmar-mehul/MMKV-XCFramework/releases/download/2.1.0/MMKVWatchExtension.xcframework.zip", // hypothetical
-			checksum: "3ebc4ce2f9cdfc502da2147150373f10946e9f1d1f69589e82ac8f2bffb513b3"
+			url: "https://github.com/parmar-mehul/MMKV-XCFramework/releases/download/2.2.2/MMKVWatchExtension.xcframework.zip", // hypothetical
+			checksum: "92c0741f58fd54b8fc44c400c033ac7e0bc91a36e1cb8a6b5bac325e67f2b281"
 		),
         .testTarget(name: "MMKVTests", dependencies: ["MMKV"]),
     ]
